775bc4f3d71c3348c9e7348bf1885bc39b5db664
job346 <job346@usask.ca>
2024-09-29 16:01:02 -0600

    reworked movement system and added screen wrap

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,6 +1,12 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var AnimatedSprite : AnimatedSprite2D
+@export var AnimatedSprite : AnimatedSprite2D
+
+var speed = 10
+var rotationSpeed = 1
+var rotationDirection = 0
+
+@onready var screenSize = get_viewport_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,19 +14,45 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if (Input.is_action_just_released("ui_select") || Input.is_action_just_released("ui_left") || Input.is_action_just_released("ui_right")):
+func _physics_process(delta: float) -> void:
+	get_input()
+	rotation += rotationDirection  * rotationSpeed * delta
+	move_and_slide()
+	screenWrap()
+
+
+func get_input():
+	if Input.is_action_just_released("ui_select") || Input.is_action_just_released("ui_left") || Input.is_action_just_released("ui_right"):
 		$PlayerSprite.play("Idle")
 		
-	if (Input.is_key_pressed(KEY_LEFT)):
-		$PlayerSprite.rotation -= delta
-		if (!Input.is_key_pressed(KEY_SPACE)) :
+	if Input.is_key_pressed(KEY_LEFT):
+		rotationDirection -= 1
+		if !Input.is_key_pressed(KEY_SPACE) :
 			$PlayerSprite.play("RotateLeft")
 		
-	if (Input.is_key_pressed(KEY_RIGHT)):
-		$PlayerSprite.rotation += delta
-		if (!Input.is_key_pressed(KEY_SPACE)) :
+	if Input.is_key_pressed(KEY_RIGHT):
+		rotationDirection += 1
+		if !Input.is_key_pressed(KEY_SPACE) :
 			$PlayerSprite.play("RotateRight")
 		
-	if (Input.is_key_pressed(KEY_SPACE)):
+	if Input.is_key_pressed(KEY_SPACE):
 		$PlayerSprite.play("Shoot")
+		if rotationDirection > 0:
+			velocity -= transform.y * speed
+		else:
+			velocity += transform.y * speed
+		
+	
+func screenWrap():
+	# horizontal wrap
+	if position.x > screenSize.x:
+		position.x = 0
+	if position.x < 0:
+		position.x = screenSize.x
+		
+	# vertical wrap
+	if position.y > screenSize.y:
+		position.y = 0
+	if position.y < 0:
+		position.y = screenSize.y
+	
